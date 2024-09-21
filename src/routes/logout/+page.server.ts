972aa9a459e5026112fd2b0f3eaf4023d6f69204
EfremovEgor <howlplaystop@gmail.com>
2024-09-21 16:26:15 +0300

    update +page.server.ts

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -5,6 +5,6 @@ export const load = async ({ cookies }) => {
     const sessionToken = cookies.get("SESSION");
     if (sessionToken == null) redirect(302, "/sign-in");
     redis.del(sessionToken);
-    cookies.delete("SESSION", { path: "/" });
+    cookies.delete("SESSION", { path: "/", secure: false });
     redirect(302, "/sign-in");
 };
